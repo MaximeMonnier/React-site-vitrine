0fb2a2d671690d740574e40fd0b1ca6697eaec30
Maxime Monnier <maximemonnier39@gmail.com>
2023-05-23 15:39:20 +0400

    initialisation project

---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <div className="notFound-content">
+        <h2>Erreur 404</h2>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
+<div className="notFound-content"></div>;
---
+++ src/pages/Project4.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Project1 = () => {
+  return (
+    <div>
+      <h1>PROJETC 4</h1>
+    </div>
+  );
+};
+
+export default Project1;
+<h1>PROJETC 1</h1>;
